168d5407aa8a3f5de30c12716c2e925005b25928
Kaung Htet Zaw <emoosx@gmail.com>
2014-06-18 05:24:14 +0800

    Project skeleton starter code for contestants
    Still getting the hang of node and express

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express'),
-    logfmt = require('logfmt');
+    logger = require('morgan'),
+    logfmt = require('logfmt'),
+    jsdom = require('jsdom'),
+    cookieParser = require('cookie-parser');
 
 var app = express();
 
@@ -10,8 +13,36 @@ app.get('/', function(req, res) {
 });
 
 app.get('/contestants', function(req, res) {
-  res.send('Contestants');
+  getContestants(function(result) {
+    res.json(200, {
+      status: 'ok',
+      contestants: result
+    });
+  });
 });
+
+function getContestants(callback) {
+  var contestants = [];
+  jsdom.env({
+    url: 'http://missuniversemyanmar.com/missunimm/2014contestants',
+    scripts: ['http://code.jquery.com/jquery.js'],
+    done: function(errors, window) {
+      var $ = window.$;
+      $('.views-row').each(function() {
+        var $girl = $(this);
+        var contestant = {
+          name: $girl.find('.views-field-title').text().trim(),
+          votes: {
+            facebook: 12,
+            website: 100
+          }
+        };
+        contestants.push(contestant);
+      });
+      callback(contestants);
+    }
+  });
+}
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
